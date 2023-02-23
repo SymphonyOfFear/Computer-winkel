724a8ba19a6fbde5cc7aed2a7e05886a3c3e63f4
Keiji <mikeykouw@hotmail.com>
2023-02-23 13:30:26 +0100

    data update 1

---
+++ site/database.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "GFG";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
--- site/manufacturers_index.php
+++ site/manufacturers_index.php
@@ -2,3 +2,31 @@
 
 
 echo "Hier komt een overzicht van alle fabrikanten";
+require 'database.php';
+
+//de sql query
+$sql = "SELECT forename, surname FROM users";
+
+//hier wordt de query uitgevoerd met de database
+$result = mysqli_query($conn,$sql);
+
+/**
+ * Hier wordt het resultaat ($result) omgezet
+ * in een *multidimensionale associatieve array
+ * in dit voorbeeld staat $all_users maar dit mag
+ * voor bijvoorbeeld producten $all_products heten.
+ * Maar dit kies je zelf
+ */
+$all_users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+
+/**
+ * Hier loop (iterate) je over alle waardes die gevonden zijn.
+ * Je kunt zoals je zien paragraaf-tags gebruiken
+ * maar je kunt ook andere HTML-**tags** gebruiken
+ */
+?>
+<?php foreach($users as $user): ?>
+  <p><?php echo $user["voornaam"] ?></p>
+<?php endforeach; ?>
